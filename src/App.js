ccc6cf257c4ee8a8db7709c72b7b03e3d70888ef
Shannon Murphy <Murphy1693@gmail.com>
2023-01-06 20:41:00 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1>
+      React with Tailwind
+      <div className="text-amber-900">Here's some text for tailwind</div>
+    </h1>
+  );
+};
+
+export default App;
