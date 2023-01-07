ce41e214194a735eb506c794c347dba44df27bac
Shannon Murphy <Murphy1693@gmail.com>
2023-01-06 22:21:36 -0800

    removed logger, added socket

--- server/index.js
+++ server/index.js
@@ -1,13 +1,36 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const logger = require("./middleware.js");
-
+// const logger = require("./middleware.js");
 const app = express();
 app.use(cors());
-app.use(logger);
-
 app.use(express.json());
+
+const socket = new WebSocket(
+  `wss://ws.finnhub.io?token=${process.env.API_KEY}`
+);
+
+socket.addEventListener("open", (event) => {
+  socket.send(JSON.stringify({ type: "subscribe", symbol: "ASTS" }));
+  socket.send(JSON.stringify({ type: "subscribe", symbol: "BINANCE:BTCUSDT" }));
+  socket.send(JSON.stringify({ type: "subscribe", symbol: "IC MARKETS:1" }));
+});
+
+socket.addEventListener("message", function (event) {
+  console.log("Message from server ", event.data);
+});
+
+var unsubscribe = function (symbol) {
+  socket.send(JSON.stringify({ type: "unsubscribe", symbol: symbol }));
+};
+
+setTimeout(() => {
+  unsubscribe("ASTS");
+  unsubscribe("BINANCE:BTCUSDT");
+  unsubscribe("IC MARKETS:1");
+}, 20000);
+
+// app.use(logger);
 
 app.use(express.static("./public"));
 
--- server/middleware.js
+++ server/middleware.js
@@ -1,8 +1,9 @@
 module.exports = (req, res, next) => {
   // console.log(req.headers);
   if (req.headers["X-Finnhub-Secret"] === "cesfcrqad3i2r4ra0nt0") {
+    console.log("finnhub request", req.headers);
     res.status(200).send();
   } else {
-    next();
+    res.status(404).send();
   }
 };
